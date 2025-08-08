38740baf31ccc7eaf12d6afd0439bd95f40acc97
Andrès Sierra <wasierran@puce.edu.ec>
2025-08-07 19:20:55 -0500

    react router y pàginas principales

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { Routes, Route } from 'react-router-dom'
+
+// Importacion de  las páginas que creadas
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Settings from './pages/Settings'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Definimos las rutas principales */}
+      <Routes>
+        <Route path="/" element={<Home />} />           {/* Página principal */}
+        <Route path="/about" element={<About />} />     {/* Sobre nosotros */}
+        <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard vacío */}
+        <Route path="/settings" element={<Settings />} />   {/* Configuración vacía */}
+      </Routes>
     </>
   )
 }
